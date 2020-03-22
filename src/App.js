38005fce2b8658dbf5bc85d277ae8c83ea20e060
crp1212 <403454318@qq.com>
2020-03-22 21:51:23 +0800

    feat: 添加修改测试

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Upload from './component/upload.jsx'
-
+console.log('123123')
+console.log('123123')
+console.log('123123')
+console.log('123123')
 class App extends Component {
 
     render() {
